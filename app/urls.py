ff2d7c51b9dbb5aa8f2ddf337e0ccafb75dc517d
JhonnyAmplitude <jhonnyamplituda@gmail.com>
2025-01-26 14:23:35 +0500

    feat: update organizations

--- app/urls.py
+++ app/urls.py
@@ -16,12 +16,12 @@ urlpatterns = [
     path('register/', register, name='user-register'),
     path('login/', login, name='user-login'),
 
-    path('organizations/', OrganizationListView.as_view(), name='organization-list'),
-    path('organizations/create/', OrganizationCreateView.as_view(), name='organization-create'),
-    path('organizations/with_users/', GetAllOrganizationsWithUsersView.as_view()),
-
     path('users/me/', CustomUserView.as_view(), name='profile-update'),
     path('users/', GetUsersAndTheirOrganizationsViewSet.as_view({'get': 'list'}), name='users-list'),
     path('users/<int:user_id>/add_organizations/', AddOrganizationsToUserView.as_view()),
     path('users/<int:user_id>/organizations/', UserOrganizationsView.as_view()),
+
+    path('organizations/', OrganizationListView.as_view(), name='organization-list'),
+    path('organizations/create/', OrganizationCreateView.as_view(), name='organization-create'),
+    path('organizations/with_users/', GetAllOrganizationsWithUsersView.as_view()),
 ]
